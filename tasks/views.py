ed352fc92e9b01c65342372261054029a4d3196f
Alisha <alishaanderssonn@icloud.com>
2025-04-22 20:26:56 +0200

    Add Task serializer and list view

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskListView(APIView):
+    """
+    API view to retrieve all tasks.
+    """
+
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
