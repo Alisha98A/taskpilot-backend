3c2f47682923bee91275166c038563d8b362b174
Alisha <alishaanderssonn@icloud.com>
2025-04-22 18:45:45 +0200

    Add file type validator

--- tasks/validators.py
+++ tasks/validators.py
@@ -10,3 +10,15 @@ def validate_file_size(value):
     max_size = 10 * 1024 * 1024
     if value.size > max_size:
         raise ValidationError('File size should not exceed 10 MB')
+
+
+def validate_file_type(value):
+    """
+    Validates that the uploaded file is of an allowed type (PDF, JPEG, PNG).
+    """
+    allowed_types = ['application/pdf', 'image/jpeg', 'image/png']
+    file_type, _ = mimetypes.guess_type(value.name)
+    if file_type not in allowed_types:
+        raise ValidationError(
+            'File type not supported. Only PDF, JPEG, and PNG are allowed.'
+        )
