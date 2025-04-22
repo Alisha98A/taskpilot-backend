36b5c423a90288736fb924b2a9f4ae8c66e9277b
Alisha <alishaanderssonn@icloud.com>
2025-04-22 18:45:07 +0200

    Add file size validator

--- tasks/validators.py
+++ tasks/validators.py
@@ -1,2 +1,12 @@
 import mimetypes
 from django.core.exceptions import ValidationError
+
+
+def validate_file_size(value):
+    """
+    Validates that the uploaded file does
+    not exceed the maximum allowed size (10 MB).
+    """
+    max_size = 10 * 1024 * 1024
+    if value.size > max_size:
+        raise ValidationError('File size should not exceed 10 MB')
