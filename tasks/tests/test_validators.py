a45919850e83e7524e7abf5b6ab919190552cf52
Alisha <alishaanderssonn@icloud.com>
2025-04-22 18:51:00 +0200

    Add test class for validator test

--- tasks/tests/test_validators.py
+++ tasks/tests/test_validators.py
@@ -2,3 +2,7 @@ from django.core.exceptions import ValidationError
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from tasks import validators
+
+
+class ValidatorTests(TestCase):
+    pass
