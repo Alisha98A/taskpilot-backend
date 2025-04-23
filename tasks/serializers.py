c858d3ec7a15e6ddf0997cce12f2a4fa5c471661
Alisha <alishaanderssonn@icloud.com>
2025-04-23 19:48:34 +0200

    Add file upload and comment to TaskSerializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 from .models import Task
 
+# Adapted from Django REST Framework walkthrough project
+# provided by Code Institute.
+# Original example used a Profile model, adapted here for Task management
+
 
 class TaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    attachment = serializers.FileField(allow_null=True, required=False)
 
     class Meta:
         model = Task
-        fields = ['id', 'title', 'description', 'due_date', 'attachment',
-                  'priority', 'category', 'state',
-                  'created_at', 'updated_at', 'owner']
+        fields = [
+            'id', 'title', 'description', 'due_date', 'attachment',
+            'priority', 'category', 'state',
+            'created_at', 'updated_at', 'owner'
+        ]
