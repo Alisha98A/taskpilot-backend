3772e2b821d78078f77fdaf0b1a577bc9532f106
Alisha <alishaanderssonn@icloud.com>
2025-04-23 20:36:31 +0200

    Restrict task access to authenticated owners only

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,10 @@
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from .models import Task
 from .serializers import TaskSerializer
+from taskpilot_api.permissions import IsOwnerOnly
 
 # Adapted from Django REST Framework walkthrough project
 # provided by Code Institute.
@@ -13,8 +14,9 @@ from .serializers import TaskSerializer
 # ---------- Task List View ----------
 class TaskListView(APIView):
     """
-    API view to retrieve all tasks.
+    API view to retrieve all tasks for the authenticated user.
     """
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
         tasks = Task.objects.all()
@@ -24,12 +26,17 @@ class TaskListView(APIView):
 
 # ---------- Task Detail View ----------
 class TaskDetail(APIView):
+    """
+    API view to retrieve, update, or delete a single task.
+    """
     serializer_class = TaskSerializer
+    permission_classes = [IsOwnerOnly]
 
     # ---------- Helper Method ----------
     def get_object(self, pk):
         try:
             task = Task.objects.get(pk=pk)
+            self.check_object_permissions(self.request, task)
             return task
         except Task.DoesNotExist:
             raise Http404
@@ -37,13 +44,15 @@ class TaskDetail(APIView):
     # ---------- Retrieve Task ----------
     def get(self, request, pk):
         task = self.get_object(pk)
-        serializer = TaskSerializer(task)
+        serializer = TaskSerializer(
+            task, context={'request': request})
         return Response(serializer.data)
 
     # ---------- Update Task ----------
     def put(self, request, pk):
         task = self.get_object(pk)
-        serializer = TaskSerializer(task, data=request.data)
+        serializer = TaskSerializer(
+            task, data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
