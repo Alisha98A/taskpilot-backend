8d958dd2e8a9ea21e265e86a373388af703a4830
Alisha <alishaanderssonn@icloud.com>
2025-04-23 20:46:54 +0200

    Add is_owner field to task serializer & DRF login/logout URL for browsable API

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -9,11 +9,16 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     attachment = serializers.FileField(allow_null=True, required=False)
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Task
         fields = [
             'id', 'title', 'description', 'due_date', 'attachment',
             'priority', 'category', 'state',
-            'created_at', 'updated_at', 'owner'
+            'created_at', 'updated_at', 'owner', 'is_owner'
         ]
