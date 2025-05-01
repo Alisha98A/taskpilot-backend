0282744de86bf004837231ae8b14abbbbc4eac3d
Alisha <alishaanderssonn@icloud.com>
2025-05-01 17:24:33 +0200

    Add admin config for Task model with filters and search

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'owner', 'due_date', 'state', 'priority')
+    list_filter = ('state', 'priority', 'category', 'owner')
+    search_fields = ('title', 'description', 'owner__username')
+    ordering = ['due_date']
