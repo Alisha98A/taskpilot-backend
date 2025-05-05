09a4af5c2d2ede953bd4cb20f354e36e9c997292
Alisha <alishaanderssonn@icloud.com>
2025-05-05 20:30:34 +0200

    Add contact form submission route

--- tasks/urls.py
+++ tasks/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
 
     path('notes/', NoteListCreateView.as_view(), name='note-list-create'),
     path('notes/<int:pk>/', NoteDetailView.as_view(), name='note-detail'),
+
+    path('contact/', ContactCreateView.as_view(), name='contact-create'),
 ]
