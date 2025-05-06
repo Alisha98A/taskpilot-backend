36710fc3b74a5e8ada9575b0d73d673f6478420f
Alisha <alishaanderssonn@icloud.com>
2025-05-06 21:27:19 +0200

    Show human-readable timestamps

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Task, Note, Contact
 from .validators import validate_file_size, validate_file_type
@@ -16,10 +17,18 @@ class TaskSerializer(serializers.ModelSerializer):
         validators=[validate_file_size, validate_file_type]
     )
     is_owner = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
     class Meta:
         model = Task
