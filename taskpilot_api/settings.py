9a157fd3899986997c8c4053d232a9b1ec07a28f
Alisha <alishaanderssonn@icloud.com>
2025-05-07 20:12:41 +0200

    Update DATABASES setting for dev and prod

--- taskpilot_api/settings.py
+++ taskpilot_api/settings.py
@@ -12,11 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists('env.py'):
     import env
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Cloudinary Media Storage
 CLOUDINARY_STORAGE = {
@@ -91,14 +93,17 @@ WSGI_APPLICATION = 'taskpilot_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
