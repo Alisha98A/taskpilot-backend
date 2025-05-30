7e8578e358c59988de05fd5f1d48d3d32366eed7
Alisha <alishaanderssonn@icloud.com>
2025-05-30 14:26:37 +0200

    register Contact model in admin panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Task, Note
+from .models import Contact
 
+admin.site.register(Contact)
 
 class NoteInline(admin.TabularInline):
     model = Note
