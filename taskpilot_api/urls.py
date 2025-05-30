1dbd50e1299c3e45265f1cb8c05a2e45ca917988
Alisha <alishaanderssonn@icloud.com>
2025-05-30 16:53:10 +0200

    Refactor URLs to serve React frontend and namespace API under /api/

--- taskpilot_api/urls.py
+++ taskpilot_api/urls.py
@@ -1,26 +1,28 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_route
+from .views import logout_route
+from django.views.generic import TemplateView
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    # Root route
-    path('', root_route),
+    # React front end served at root
+    path('', TemplateView.as_view(template_name='index.html')),
     
-    # Admin route
+    # Admin panel
     path('admin/', admin.site.urls),
     
-    # DRF Authentication (Login/Logout)
-    path('api-auth/', include('rest_framework.urls')),  # For browsing the API
+    # API routes - all prefixed with /api/
+    path('api/api-auth/', include('rest_framework.urls')),  # For browsing the API
     
-    # DJ Rest Auth - Login/Logout and Password Reset
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),  # Login/Logout
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),  # Registration
+    path('api/dj-rest-auth/logout/', logout_route),
+    path('api/dj-rest-auth/', include('dj_rest_auth.urls')),    
+    path('api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),  # Registration
     
-    # JWT Token Obtain and Refresh views
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     
     # Include tasks app routes
     path('api/', include('tasks.urls')),
 ]
+
+handler404 = TemplateView.as_view(template_name='index.html')
