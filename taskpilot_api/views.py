37fead368a99a4686ef5e132fc63a6ac5e86e480
Alisha <alishaanderssonn@icloud.com>
2025-05-30 17:05:25 +0200

    Add root_route and logout_route API views

--- taskpilot_api/views.py
+++ taskpilot_api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import logout
 
 
 @api_view()
@@ -7,3 +9,9 @@ def root_route(request):
     return Response({
         "message": "Welcome to my DRF API!"
     })
+
+
+@api_view(['POST'])
+def logout_route(request):
+    logout(request)
+    return Response({"message": "Successfully logged out."}, status=status.HTTP_200_OK)
