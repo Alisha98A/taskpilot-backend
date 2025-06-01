4d0def71d0112eb29dff5d9aac360b49dadbfa08
Alisha <alishaanderssonn@icloud.com>
2025-06-01 09:37:49 +0200

    Refactor URLs for PEP8 compliance and line length limits

--- taskpilot_api/urls.py
+++ taskpilot_api/urls.py
@@ -2,24 +2,41 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from rest_framework_simplejwt import views as jwt_views
-from .views import logout_route 
+from .views import logout_route
 
 urlpatterns = [
     # React front end served at root
     path('', TemplateView.as_view(template_name='index.html')),
-    
+
     # Admin panel
     path('admin/', admin.site.urls),
 
     # API
     path('api/api-auth/', include('rest_framework.urls')),
-    path('api/dj-rest-auth/logout/', logout_route, name='logout'),
-    path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    
+    path(
+        'api/dj-rest-auth/logout/',
+        logout_route,
+        name='logout',
+    ),
+    path(
+        'api/dj-rest-auth/',
+        include('dj_rest_auth.urls'),
+    ),
+    path(
+        'api/dj-rest-auth/registration/',
+        include('dj_rest_auth.registration.urls'),
+    ),
+    path(
+        'api/token/',
+        jwt_views.TokenObtainPairView.as_view(),
+        name='token_obtain_pair',
+    ),
+    path(
+        'api/token/refresh/',
+        jwt_views.TokenRefreshView.as_view(),
+        name='token_refresh',
+    ),
+
     # Include tasks app routes
     path('api/', include('tasks.urls')),
 ]
